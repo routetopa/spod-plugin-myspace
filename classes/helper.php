625658d2350fa1b46e1013c9bf5e22ce7509e558
isisadmin <isislab.unisa@gmail.com>
2015-10-29 18:05:07 +0100

    private room

---
+++ classes/helper.php
@@ -0,0 +1,56 @@
+<?php
+
+class SPODPR_CLASS_Helper
+{
+    private static $classInstance;
+
+    public static function getInstance()
+    {
+        if(self::$classInstance === null)
+        {
+            self::$classInstance = new self();
+        }
+
+        return self::$classInstance;
+    }
+
+    public function getUserPrivateRoom($userId)
+    {
+        return $this->processCard(SPODPR_BOL_Service::getInstance()->getUserPrivateRoom($userId));
+    }
+
+    private function processCard($bolCards)
+    {
+        $cards = array();
+
+        foreach($bolCards as $bolCard)
+        {
+            $card = new SPODPR_CLASS_Card();
+            $card->cardId = $bolCard->id;
+            $card->isDatalet = false;
+            $card->ownerId = $bolCard->ownerId;
+            $card->cardType = $bolCard->cardType;
+            $card->card = json_decode($bolCard->card);
+
+            if($card->cardType == 'datalet')
+            {
+                $datalet = ODE_BOL_Service::getInstance()->getDataletById($card->card->dataletId);
+                if($datalet)
+                {
+                    $card->isDatalet = true;
+                    $card->dataletId = $datalet->id;
+                    $card->component = $datalet->component;
+                    $card->data = $datalet->data;
+                    $card->fields = $datalet->fields;
+                    $card->params = json_decode($datalet->params);
+                    $card->params->dataUrl = $card->params->{'data-url'};
+                    $card->preset = $datalet->params;
+                }
+            }
+
+            array_push($cards, $card);
+
+        }
+        return $cards;
+    }
+}
--- controllers/main.php
+++ controllers/main.php
@@ -7,7 +7,7 @@ class SPODPR_CTRL_Main extends OW_ActionController
     {
         OW::getDocument()->addStyleSheet(OW::getPluginManager()->getPlugin('spodpr')->getStaticUrl() . 'css/private_room.css');
         OW::getDocument()->getMasterPage()->setTemplate(OW::getPluginManager()->getPlugin('spodpr')->getRootDir() . 'master_pages/general.html');
-        $this->assign('cards', $this->processCard(SPODPR_BOL_Service::getInstance()->getUserPrivateRoom(OW::getUser()->getId())));
+        $this->assign('cards', SPODPR_CLASS_Helper::getInstance()->getUserPrivateRoom(OW::getUser()->getId()));
 
         $this->assign('components_url', SPODPR_COMPONENTS_URL);
         $this->assign('plugin_root_dir', OW::getPluginManager()->getPlugin('spodpr')->getStaticUrl());
@@ -28,40 +28,4 @@ class SPODPR_CTRL_Main extends OW_ActionController
 
         OW::getDocument()->addOnloadScript($js);
     }
-
-    private function processCard($bolCards)
-    {
-        $cards = array();
-
-        foreach($bolCards as $bolCard)
-        {
-            $card = new SPODPR_CLASS_Card();
-            $card->cardId = $bolCard->id;
-            $card->isDatalet = false;
-            $card->ownerId = $bolCard->ownerId;
-            $card->cardType = $bolCard->cardType;
-            $card->card = json_decode($bolCard->card);
-
-            if($card->cardType == 'datalet')
-            {
-                $datalet = ODE_BOL_Service::getInstance()->getDataletById($card->card->dataletId);
-                if($datalet)
-                {
-                    $card->isDatalet = true;
-                    $card->dataletId = $datalet->id;
-                    $card->component = $datalet->component;
-                    $card->data = $datalet->data;
-                    $card->fields = $datalet->fields;
-                    $card->params = json_decode($datalet->params);
-                    $card->params->dataUrl = $card->params->{'data-url'};
-                    $card->preset = $datalet->params;
-                }
-            }
-
-            array_push($cards, $card);
-
-        }
-        return $cards;
-    }
-
 }
