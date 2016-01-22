19d219bebba7fb6200c872c7a332ec8b4017ca3a
andrpet <andrpet@gmail.com>
2016-01-22 14:58:49 +0100

    tooltip internationalization

--- components/private_room_card.php
+++ components/private_room_card.php
@@ -2,12 +2,13 @@
 
 class SPODPR_CMP_PrivateRoomCard extends OW_Component
 {
-    public function __construct($cssClass, $selectedType=array())
+    public function __construct($cssClass, $selectedType=array(), $pluginKey="")
     {
         parent::__construct();
 
         $this->assign('components_url', SPODPR_COMPONENTS_URL);
         $this->assign('css_class', $cssClass);
         $this->assign('cards', SPODPR_CLASS_Helper::getInstance()->getUserPrivateRoom(OW::getUser()->getId(),$selectedType));
+        $this->assign('pluginKey', $pluginKey);
     }
 }
