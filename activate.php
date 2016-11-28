edf29e1ab028fa007826598d671fe2570d1ba605
rended <renato.dedonato@gmail.com>
2016-11-28 16:15:36 +0100

    removed admin settings

--- activate.php
+++ activate.php
@@ -1,4 +1,3 @@
 <?php
 
 OW::getNavigation()->addMenuItem(OW_Navigation::MAIN, 'spodpr.index', 'spodpr', 'main', OW_Navigation::VISIBLE_FOR_MEMBER);
-OW::getPluginManager()->addPluginSettingsRouteName('spodpr', 'spodpr-settings');
--- init.php
+++ init.php
@@ -5,4 +5,3 @@ $spodpr_components_url = empty($preference) ? "http://deep.routetopa.eu/COMPONEN
 define("SPODPR_COMPONENTS_URL", $spodpr_components_url);
 
 OW::getRouter()->addRoute(new OW_Route('spodpr.index', 'spodpr', "SPODPR_CTRL_Main", 'index'));
-OW::getRouter()->addRoute(new OW_Route('spodpr-settings', '/spodpr/settings', 'SPODPR_CTRL_Admin', 'settings'));
