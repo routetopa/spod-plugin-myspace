b4292ac340d96e4284e6d9fead7cdd1c67de8d5a
andrpet <andrpet@gmail.com>
2016-11-28 17:15:09 +0100

    added new datalet/controllet to DEEP configuration XML

--- init.php
+++ init.php
@@ -1,7 +1,3 @@
 <?php
 
-$preference = BOL_PreferenceService::getInstance()->findPreference('spodpr_components_url');
-$spodpr_components_url = empty($preference) ? "http://deep.routetopa.eu/COMPONENTS/" : $preference->defaultValue;
-define("SPODPR_COMPONENTS_URL", $spodpr_components_url);
-
 OW::getRouter()->addRoute(new OW_Route('spodpr.index', 'spodpr', "SPODPR_CTRL_Main", 'index'));
