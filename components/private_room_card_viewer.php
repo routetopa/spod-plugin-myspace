d36d046de8c124d179bf9d69c2dfe4a905b335eb
andrpet <andrpet@gmail.com>
2017-03-16 17:45:45 +0100

    agora enhancement

--- components/private_room_card_viewer.php
+++ components/private_room_card_viewer.php
@@ -6,7 +6,22 @@ class SPODPR_CMP_PrivateRoomCardViewer extends OW_Component
     {
         parent::__construct();
 
+        $cards = SPODPR_CLASS_Helper::getInstance()->getUserPrivateRoom(OW::getUser()->getId(),$selectedType);
         $this->assign('components_url', SPODPR_COMPONENTS_URL);
-        $this->assign('cards', SPODPR_CLASS_Helper::getInstance()->getUserPrivateRoom(OW::getUser()->getId(),$selectedType));
+        $this->assign('card_definition', $this->get_datalet_definition($cards));
+        $this->assign('cards', $cards);
+    }
+
+    public function get_datalet_definition($cards)
+    {
+        $definition = [];
+
+        foreach ($cards as $card)
+        {
+            if(!in_array($card->component, $definition))
+                array_push($definition, $card->component);
+        }
+
+        return $definition;
     }
 }
